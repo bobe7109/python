5b177246009be9346f133ccf28663bbc0f9fb3ef
bobe7109 <80086381+bobe7109@users.noreply.github.com>
2021-04-09 15:55:40 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,11 @@
+fstName = ['K','i','m']
+sndName = ["Hee","Sook"]
+stdNum = [2, 0, 2, 1, 0, 6, 0, 4, 0]
+
+print(fstName)
+print(sndName)
+# 화면에 K, H, S를 출력하는 프로그램을 작성시오.
+print(fstName[0]) # K
+print(sndName[0][0]) # H
+print(sndName[1][0]) # S
+
